df1e1345273e937f5b2d34ded14755007d307607
AdrianRussell <adrianrussell@iinet.net.au>
2011-07-03 15:32:08 +1000

    Initial commit - base project structure

---
+++ SimpleQueue.Test/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleQueue.Test
+{
+    public class Class1
+    {
+    }
+}
---
+++ SimpleQueue/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleQueue
+{
+    public class Class1
+    {
+    }
+
+    internal interface ISimpleQueue
+    {
+        void Send();
+        void Receive();
+
+    }
+}
