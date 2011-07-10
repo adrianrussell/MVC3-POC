d05a392237e3fbf08e3fa20021bfa72d0252c73a
AdrianRussell <adrianrussell@iinet.net.au>
2011-07-10 22:10:42 +1000

    Initial Implementation of ApproverService MessageAgent

---
+++ ApproverService.Test/MessageAgentFixture.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using NUnit.Framework;
+using Rhino.Mocks;
+using SimpleQueue;
+
+namespace ApproverService.Test
+{
+    [TestFixture]
+    public class MessageAgentFixture
+    {
+        private ISimpleMessageQueue _messageQueue;
+
+        [SetUp]
+        public void SetUp() {
+            _messageQueue = MockRepository.GenerateMock<ISimpleMessageQueue>();
+        }
+
+        [Test]
+        public void CanCreate() {
+            var agent = GetReceiver();
+
+            Assert.That(agent, Is.Not.Null);
+        }
+
+        [Test]
+        public void Receive_MessageQueueEmpty_ReturnsEmptyMessage() {
+            var agent = GetReceiver();
+            ExpectCallOnMessageQueueReturnsEmptyMessage();
+
+            var message = agent.Receive();
+
+            Assert.That(message, Is.EqualTo(Guid.Empty));
+        }
+
+        [Test]
+        public void Recevie_MessageQueueWithMessageOnQueue_ReturnsMessage() {
+            var agent = GetReceiver();
+            var messageToReceive = Guid.NewGuid();
+            ExpectCallOnMessageQueueReturnsMessage(messageToReceive);
+
+            var message = agent.Receive();
+
+            Assert.That(message, Is.EqualTo(messageToReceive));
+        }
+
+        private void ExpectCallOnMessageQueueReturnsEmptyMessage() {
+            _messageQueue.Expect(x => x.Receive()).Return(Guid.Empty);
+        }
+
+        private void ExpectCallOnMessageQueueReturnsMessage(Guid mesageToReturn) {
+            _messageQueue.Expect(x => x.Receive()).Return(mesageToReturn);
+        }
+
+        private MessageAgent GetReceiver() {
+            return new MessageAgent(_messageQueue);
+        }
+    }
+}
---
+++ ApproverService/MessageAgent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SimpleQueue;
+
+namespace ApproverService
+{
+    public class MessageAgent
+    {
+        private readonly ISimpleMessageQueue _messageQueue;
+
+        public MessageAgent(ISimpleMessageQueue messageQueue) {
+            _messageQueue = messageQueue;
+        }
+
+        public Guid Receive() {
+            return _messageQueue.Receive();
+        }
+    }
+}
